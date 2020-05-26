44d8829a0fab0a77139aad779b46bdebd1439f16
getchipman <chipman@tutanota.com>
2020-05-26 00:56:24 -0300

    tdd for CRUD

--- test/integration/routes/products_spec.js
+++ test/integration/routes/products_spec.js
@@ -4,6 +4,8 @@ describe('Routes: Products', () => {
     let request;
     let app;
 
+    const defaultId = '56cb91bdc3464f14678934ca';
+
     const defaultProduct = { 
         name: 'Default product',
         description: 'product description',
@@ -11,7 +13,7 @@ describe('Routes: Products', () => {
     };
     const expectedProduct = {
         __v:0,
-        _id: '56cb91bdc3464f14678934ca',
+        _id: defaultId,
         name: 'Default product',
         description: 'product description',
         price: 100
@@ -26,7 +28,7 @@ describe('Routes: Products', () => {
         await Product.deleteMany();
 
         const product = new Product(defaultProduct);
-        product._id = '56cb91bdc3464f14678934ca';
+        product._id = defaultId;
         return await product.save();
     });
 
@@ -38,10 +40,78 @@ describe('Routes: Products', () => {
         it('should return a list of products', done => {
 
             request
-            .get('/products')
-            .end((err, res) => {
-                expect(res.body[0]).to.eql(expectedProduct);
-                done(err);
+                .get('/products')
+                .end((err, res) => {
+                    expect(res.body[0]).to.eql(expectedProduct);
+                    done(err);
+                });
+        });
+        context('when an id is specified', done => {
+            it('should return 200 with one product', done => {
+                request
+                    .get(`/products/${defaultId}`)
+                    .end((err, res) => {
+                        expect(res.statusCode).to.eql(200);
+                        expect(res.body).to.eql([expectedProduct]);
+                        done(err);
+                    });
+            });
+        });
+    });
+
+    describe('POST /products', () => {
+        context('when posting a product', () => {
+            it('should return a new product with status code 201', done => {
+                const customId = '56cb91bdc3464f14678934ba';
+                const newProduct = Object.assign({}, { _id: customId, __v: 0 }, defaultProduct);
+                const expectedSavedProduct = {
+                    __v: 0,
+                    _id: customId,
+                    name: 'Default product',
+                    description: 'product description',
+                    price: 100
+                };
+
+                request
+                    .post('/products')
+                    .send(newProduct)
+                    .end((err, res) => {
+                        expect(res.statusCode).to.eql(201);
+                        expect(res.body).to.eql(expectedSavedProduct);
+                        done(err);
+                    });
+            });
+        });
+    });
+
+    describe('PUT /products/:id', () => {
+        context('when editing a product', () => {
+            it('should update the product and return 200 as status code', done => {
+                const customProduct = {
+                    name: 'Custom name'
+                };
+                const updatedProduct = Object.assign({}, customProduct, defaultProduct)
+
+                request
+                    .put(`/products/${defaultId}`)
+                    .send(updatedProduct)
+                    .end((err, res) => {
+                        expect(res.status).to.eql(200);
+                        done(err);
+                    });
+            });
+        });
+    });
+
+    describe('DELETE /products/:id', () => {
+        context('when deleting a product', () => {
+            it('should delete a product and return 204 as status code', done => {
+                request
+                    .delete(`/products/${defaultId}`)
+                    .end((err, res) => {
+                        expect(res.status).to.eql(204);
+                        done(err);
+                    });
             });
         });
     });
